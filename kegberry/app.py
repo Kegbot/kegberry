a013661e70b1debabc42e7dd8b0c0b1207d96d5d
mike wakerly <opensource@hoho.com>
2015-01-27 19:26:37 -0500

    Bump kegbot-server to v1.2.3

--- kegberry/app.py
+++ kegberry/app.py
@@ -56,7 +56,7 @@ gflags.DEFINE_string('mysql_password', '',
 gflags.DEFINE_boolean('upgrade_system_packages', False,
     'If set, performs "apt-get upgrade" during install/upgrade.')
 
-gflags.DEFINE_string('kegbot_server_package', 'kegbot==1.1.1',
+gflags.DEFINE_string('kegbot_server_package', 'kegbot==1.2.3',
     '(Advanced use only.) Version of Kegbot Server to install.')
 
 gflags.DEFINE_string('kegbot_pycore_package', 'kegbot-pycore==1.2.0',
