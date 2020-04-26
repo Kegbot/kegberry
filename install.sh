e411f1e7ce337cb00925907ddc14f9385e896dca
mike wakerly <opensource@hoho.com>
2020-04-26 17:44:51 -0400

    Rewrite as shell script using Docker.

--- install.sh
+++ install.sh
@@ -3,10 +3,232 @@
 # Source: https://github.com/Kegbot/kegberry
 
 set -e
-set -x
 
-sudo bash -c "DEBIAN_FRONTEND=noninteractive apt-get -yq install python-setuptools"
-sudo easy_install pip
-sudo pip install -U virtualenv
-sudo pip install -U --pre kegberry
-kegberry $INSTALLFLAGS install
+COMPOSE_TEMPLATE="
+version: '3.0'
+
+services:
+  kegbot:
+    image: kegbot/server:latest
+    ports:
+      - '8000:8000'
+    volumes:
+      - _kegbot_data_dir_:/kegbot-data
+    environment:
+      KEGBOT_REDIS_URL: 'redis://redis:6379/0'
+      KEGBOT_DATABASE_URL: 'mysql://kegbot_dev:changeme@mysql/kegbot_dev'
+      KEGBOT_SETUP_ENABLED: 'true'
+      KEGBOT_DEBUG: 'true'
+
+  workers:
+    image: kegbot/server:latest
+    command: bin/kegbot run_workers
+    volumes:
+      - _kegbot_data_dir_:/kegbot-data
+    environment:
+      KEGBOT_REDIS_URL: redis://redis:6379/0
+      KEGBOT_DATABASE_URL: mysql://kegbot_dev:changeme@mysql/kegbot_dev
+      KEGBOT_SETUP_ENABLED: 'true'
+      KEGBOT_DEBUG: 'true'
+
+  mysql:
+    image: linuxserver/mariadb:latest
+    restart: always
+    environment:
+      MYSQL_ROOT_PASSWORD: 'changeme'
+      MYSQL_USER: 'kegbot_dev'
+      MYSQL_PASSWORD: 'changeme'
+      MYSQL_DATABASE: 'kegbot_dev'
+    volumes:
+      - _mysql_data_dir_:/var/lib/mysql
+
+  redis:
+    image: redis:latest
+    restart: always
+
+volumes:
+  mysql-data:
+  kegbot-data:
+"
+
+log() {
+    echo "--- $@"
+}
+
+warning() {
+    echo "WARNING: $@"
+}
+
+ensure_docker() {
+    log "Checking for docker ..."
+    docker_status=`which docker`
+    if [ $? -ne 0 ]; then
+        log "Docker not installed, installing ..."
+        curl -sSL https://get.docker.com | sh
+        sudo usermod -aG docker $USER
+    fi
+
+    docker_version=`docker --version`
+    if [ $? -ne 0 ]; then
+        log "Docker version could not be determined"
+    else
+        log "Docker version ${docker_version} - great!"
+    fi
+
+    log "Testing docker ..."
+    docker run hello-world 2>&1 > /dev/null
+    log "Docker works!"
+}
+
+ensure_python() {
+    log "Checking for python ..."
+    python_status=`which python3`
+    if [ $? -ne 0 ]; then
+        log "Python not installed, installing ..."
+        sudo apt-get install -y libffi-dev libssl-dev
+        sudo apt-get install -y python3 python3-pip
+        sudo apt-get remove python-configparser || true
+    fi
+
+    python_version=`python3 --version 2>&1`
+    if [ $? -ne 0 ]; then
+        log "Python3 version could not be determined"
+        exit 1
+    fi
+    log "Python version ${python_version} - great!"
+}
+
+ensure_docker_compose() {
+    log "Checking for docker-compose ..."
+    docker_compose_status=`which docker-compose`
+    if [ $? -ne 0 ]; then
+        log "docker-compose not installed, installing ..."
+        sudo pip3 install docker-compose
+    fi
+
+    docker_compose=`docker-compose --version`
+    if [ $? -ne 0 ]; then
+        log "docker-compose version could not be determined"
+    else
+        log "docker-compose version ${docker_compose} - great!"
+    fi
+}
+
+ensure_all_dependencies() {
+    ensure_docker
+    ensure_python
+    ensure_docker_compose
+}
+
+setup_vars() {
+    DISTRO_NAME=$(lsb_release -s -i 2>/dev/null || echo -n "Unknown")
+
+    if [ "${DISTRO_NAME}" != "Raspbian" ]; then
+        warning "Kegberry is meant to be run on Raspbian, but your distro is '${DISTRO_NAME}'"
+        read -p "Proceed anyway? [y/N]: " proceed_str
+        proceed_str=${proceed_str:-No}
+        case "${proceed_str}" in
+            [yY][eE][sS]|[yY])
+                true
+                ;;
+            *)
+                log "Exiting."
+                exit 1
+                ;;
+        esac
+    fi
+
+    KEGBERRY_DIR="$HOME/kegberry"
+    if [[ -d "${KEGBERRY_DIR}" ]]; then
+        echo "Found a Kegbot directory at ${KEGBERRY_DIR}"
+        read -p "Use this as the kegberry dir? [n/Y]: " proceed_str
+        proceed_str=${proceed_str:-Yes}
+        case "${proceed_str}" in
+            [yY][eE][sS]|[yY])
+                ;;
+            *)
+                log "Exiting."
+                exit 1
+                ;;
+        esac
+    fi
+
+    KEGBOT_DATA_DIR="${KEGBERRY_DIR}/data"
+    if [[ -d "${KEGBOT_DATA_DIR}" ]]; then
+        echo "Found a Kegbot data directory at ${KEGBOT_DATA_DIR}"
+        read -p "Proceed and use this as data dir? [n/Y]: " proceed_str
+        proceed_str=${proceed_str:-Yes}
+        case "${proceed_str}" in
+            [yY][eE][sS]|[yY])
+                ;;
+            *)
+                log "Exiting."
+                exit 1
+                ;;
+        esac
+    fi
+
+    MYSQL_DATA_DIR="${KEGBERRY_DIR}/mysql"
+    if [[ -d "${MYSQL_DATA_DIR}" ]]; then
+        echo "Found a mysql data directory at ${MYSQL_DATA_DIR}"
+        read -p "Proceed and use this as mysql dir? [n/Y]: " proceed_str
+        proceed_str=${proceed_str:-Yes}
+        case "${proceed_str}" in
+            [yY][eE][sS]|[yY])
+                ;;
+            *)
+                log "Exiting."
+                exit 1
+                ;;
+        esac
+    fi
+
+    DOCKER_COMPOSE_FILE="${KEGBERRY_DIR}/docker-compose.yml"
+    if [[ -e "${DOCKER_COMPOSE_FILE}" ]]; then
+        warning "Docker compose config already exists at ${DOCKER_COMPOSE_FILE}."
+        read -p "Proceed anyway and overwrite? [y/N]: " proceed_str
+        proceed_str=${proceed_str:-No}
+        case "${proceed_str}" in
+            [yY][eE][sS]|[yY])
+                true
+                ;;
+            *)
+                log "Exiting."
+                exit 1
+                ;;
+        esac
+    fi
+}
+
+do_install() {
+    setup_vars
+    ensure_all_dependencies
+
+    log "Installing ..."
+
+    log "Setting up directories ..."
+    mkdir -p ${KEGBOT_DATA_DIR}
+    mkdir -p ${KEGBERRY_DIR}
+    mkdir -p ${MYSQL_DATA_DIR}
+
+    log "Installing docker compose file at ${DOCKER_COMPOSE_FILE} ..."
+    echo -e "${COMPOSE_TEMPLATE}" > ${DOCKER_COMPOSE_FILE}
+    sed -i -e "s%_kegbot_data_dir_%${KEGBOT_DATA_DIR}%" ${DOCKER_COMPOSE_FILE}
+    sed -i -e "s%_mysql_data_dir_%${MYSQL_DATA_DIR}%" ${DOCKER_COMPOSE_FILE}
+
+    log "Done!"
+    echo ""
+    echo "To run, do the following: "
+    echo ""
+    echo "    $ cd ${KEGBERRY_DIR}"
+    echo "    $ docker-compose up"
+    echo ""
+    echo "To run in the background, use:"
+    echo ""
+    echo "    $ docker-compose up -d"
+    echo ""
+    echo "Enjoy!"
+
+}
+
+do_install
